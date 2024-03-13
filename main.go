0831002d38d0e3b92e0a23173b5aeb9725353e29
Hearts Fiddlesticks <heartsfi@un-c4.kzn.21-school.ru>
2024-03-13 10:47:08 +0300

    sql added

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
+)
 
+func main() {
+	connStr := "user=server password=server dbname=userdb sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err := db.Exec("INSERT INTO usertable (id, login, password) VALUES (2, 'user1', 'pass1')")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(rows.RowsAffected())
 }
